f47be9a8e2380292c44c59377a7ef3f719b5c5a0
DESKTOP-V0P3D10\Adryan <adryanberca@yahoo.com>
2016-11-06 20:22:52 +0200

    Created controllers - without functionality for the moment

---
+++ PSSC/University.Web/Controllers/StudyYearController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace University.Web.Controllers
+{
+    public class StudyYearController : Controller
+    {
+        // GET: StudyYear
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: StudyYear/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudyYear/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudyYear/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudyYear/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudyYear/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudyYear/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudyYear/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
